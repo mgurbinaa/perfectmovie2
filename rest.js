c1fc617ec1105173d42d2570444e74df3ebc59b7
Miguel Angel Urbina <aurbina24397@outlook.com>
2017-12-20 22:50:23 -0600

    Version final

--- rest.js
+++ rest.js
@@ -36,7 +36,7 @@ REST_ROUTER.prototype.handleRoutes = function(router, connection, md5) {
 	router.post("/getDiscover", function(req, res){
 		var user = req.body.u;
 		var fgenre = req.body.g;
-		if(fgenre==null){
+		if(fgenre=='null'){
 			console.log("fg null");
 			var query = "SELECT COUNT(*) FROM likes WHERE user = (SELECT idUser FROM users WHERE id = '"+user+"');";
 			connection.query(query, function(err, rows){
